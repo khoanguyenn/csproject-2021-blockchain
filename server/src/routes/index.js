e96c71530ae4685d13fc692b5785d2c8386a6edc
haxuanhuy <52565807+haxuanhuy@users.noreply.github.com>
2021-10-14 00:07:27 +0700

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,10 +3,11 @@ const AuthController = require("../controllers/AuthController");
 const TestController = require('../controllers/TestController');
 const DistributorController = require("../controllers/distributor");
 const MedicalUnitController = require("../controllers/medicalunit");
+const ManufacturerController=require("../controllers/manufacturer");
 
 const initApp = (app) => {
     app.use("/auth",  AuthController)
-    app.use("/manufacturer", (req, res) => {console.log("Manufacturer route is here!"); res.send("manufacturer")});
+    app.use("/manufacturer", ManufacturerController);
     app.use("/distributor", DistributorController)
     app.use("/medical-unit", MedicalUnitController)
 
